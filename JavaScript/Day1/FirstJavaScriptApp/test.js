ab546466c93b36f29517bf0c4cc75446ee11cb42
ChengFong <xt3120932@gmail.com>
2021-03-26 19:11:32 +0800

    更新

---
+++ JavaScript/Day1/FirstJavaScriptApp/test.js
@@ -0,0 +1,14 @@
+let name = "Mary";
+{
+    let name = "Kevin";
+    console.log(name);
+}
+console.log(name);
+
+
+var name = "Mary";
+{
+    var name = "Kevin";
+    console.log(name);
+}
+console.log(name);
